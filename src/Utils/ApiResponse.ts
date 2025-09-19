740677282484a1fff8f5dbde0b6e04e551ba00d0
Raheel1258 <rahilahmad1258@gmail.com>
2025-09-19 17:04:38 +0500

    add api response type

--- src/Utils/ApiResponse.ts
+++ src/Utils/ApiResponse.ts
@@ -1,9 +1,22 @@
-// class ApiResponse {
-//   constructor(statusCode, data, message = 'Succcess') {
-//     this.statusCode = statusCode;
-//     this.data = data;
-//     this.message = message;
-//     this.success = statusCode < 400;
-//   }
-// }
-// export { ApiResponse };
+  /**
+   * @param statusCode - The status code of the response
+   * @param data - The data of the response
+   * @param message - The message of the response
+   * @param success - The success of the response
+   * @param errors - The errors of the response
+   */
+class ApiResponse<TData = unknown> {
+  public statusCode: number;
+  public data: TData;
+  public message: string;
+  public success: boolean;
+
+  constructor(statusCode: number, data: TData, message: string = 'Success') {
+    this.statusCode = statusCode;
+    this.data = data;
+    this.message = message;
+    this.success = statusCode < 400;
+  }
+}
+
+export { ApiResponse };
