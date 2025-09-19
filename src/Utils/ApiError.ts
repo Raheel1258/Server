f9f4b7c76db429a7ca6a84d0976184a6b2019549
Raheel1258 <rahilahmad1258@gmail.com>
2025-09-19 17:06:30 +0500

    add api error

--- src/Utils/ApiError.ts
+++ src/Utils/ApiError.ts
@@ -1,17 +1,35 @@
-// class ApiError extends Error {
-//   constructor(statusCode, message, errors = [], stack = '') {
-//     super(message);
-//     this.statusCode = statusCode;
-//     this.data = null;
-//     this.message = message;
-//     this.success = false;
-//     thiss.errors = errors;
+/**
+ * @param statusCode - The status code of the error
+ * @param message - The message of the error
+ * @param errors - The errors of the error
+ * @param stack - The stack of the error
+ */
+class ApiError extends Error {
+  public statusCode: number;
+  public data: unknown;
+  public success: false;
+  public errors: unknown[];
 
-//     if (stack) {
-//       this.stack = stack;
-//     } else {
-//       Error.captureStackTrace(this, this.constructor);
-//     }
-//   }
-// }
-// export { ApiError };
+  constructor(
+    statusCode: number,
+    message: string,
+    errors: unknown[] = [],
+    stack?: string,
+  ) {
+    super(message);
+
+    this.name = 'ApiError';
+    this.statusCode = statusCode;
+    this.data = null;
+    this.success = false;
+    this.errors = errors;
+
+    if (stack) {
+      this.stack = stack;
+    } else if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, ApiError);
+    }
+  }
+}
+
+export { ApiError };
