998af7e5f8771ae829978be83f676d25727964dc
Raheel1258 <rahilahmad1258@gmail.com>
2025-09-20 19:41:12 +0500

    add moderesolution

--- src/Controller/NewsLetter.Controller.ts
+++ src/Controller/NewsLetter.Controller.ts
@@ -1,22 +1,21 @@
-import prisma from 'DB/Connection';
+import prisma from '../DB/Connection';
 import { Request, Response } from 'express';
-import newsletterSchema from 'Schema/Newsletter.Schema';
-import { ApiResponse } from 'Utils/ApiResponse';
-import { ApiError } from 'Utils/ApiError';
+import newsletterSchema from '../Schema/Newsletter.Schema';
+import { ApiResponse } from '../Utils/ApiResponse';
+import { ApiError } from '../Utils/ApiError';
 
-type NewsLetterRequest = Request<Record<string, never>, Record<string, never>, { email: string }>;
-export const createNewsLetter = async (req: NewsLetterRequest, res: Response) => {
+export const createNewsLetter = async (req: Request, res: Response) => {
   try {
     const { email } = newsletterSchema.parse(req.body);
     const subscription = await prisma.newsletter.create({
       data: { email },
     });
-    return res.json(new ApiResponse(201, subscription, 'Newsletter subscription successful'));
+    return res.status(201).json(new ApiResponse(201, subscription, 'Newsletter subscription successful'));
   } catch (error: unknown) {
     if (error instanceof ApiError) {
       return res.status(error.statusCode).json(new ApiResponse(error.statusCode, error.message));
     }
 
-    return res.status(400).json(new ApiError(400, error instanceof Error ? error.message : 'Bad Request'));
+    return res.status(400).json(new ApiResponse(400, error, error instanceof Error ? error.message : 'Bad Request'));
   }
 };
